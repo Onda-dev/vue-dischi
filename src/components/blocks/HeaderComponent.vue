6cc3ce909cac113b4b4c5605c59a662f55b2eb83
Onda-dev <manuelaltieri3@gmail.com>
2022-06-07 15:55:26 +0200

    Header inserito

---
+++ src/components/blocks/HeaderComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+      <div class="container">
+          <div class="logo col-1 p-3">
+              <img src="/spotify-logo.png" alt="logo">
+          </div>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    background-color: #2E3A46;
+}
+img {
+    width: 100%;
+}
+</style>
