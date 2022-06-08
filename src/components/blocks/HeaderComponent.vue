07c8258542c85c3ab627f79d39e09455c961d84b
Onda-dev <manuelaltieri3@gmail.com>
2022-06-08 15:18:52 +0200

    Aggiunta select

--- src/components/blocks/HeaderComponent.vue
+++ src/components/blocks/HeaderComponent.vue
@@ -4,6 +4,15 @@
           <div class="logo col-1 p-3">
               <img src="/spotify-logo.png" alt="logo">
           </div>
+          <div class="filter-select">
+              <select name="genre" id="filter">
+                    <option value="genre" disabled selected>Genre</option>
+                    <option value="pop">Pop</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="metal">Metal</option>
+                    <option value="rock">Rock</option>
+              </select>
+          </div>
       </div>
   </header>
 </template>
@@ -17,6 +26,11 @@ export default {
 <style lang="scss" scoped>
 header {
     background-color: #2E3A46;
+    .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 }
 img {
     width: 100%;
